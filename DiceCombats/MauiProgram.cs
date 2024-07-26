e2ba5fe70dc57477a31fb5324d3170559d323ed2
YannCharbon <yann.charbon@ik.me>
2024-07-26 21:09:11 +0200

    Maximize app at startup

--- DiceCombats/MauiProgram.cs
+++ DiceCombats/MauiProgram.cs
@@ -1,5 +1,11 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
 using MudBlazor.Services;
+
+#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+#endif
 
 namespace DiceCombats
 {
@@ -13,6 +19,28 @@ namespace DiceCombats
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                })
+
+                // Add maximize window on startup
+                .ConfigureLifecycleEvents(events =>
+                {
+#if WINDOWS
+                    events.AddWindows(w =>
+                    {
+                        w.OnWindowCreated(window =>
+                        {
+                            window.ExtendsContentIntoTitleBar = true; //If you need to completely hide the minimized maximized close button, you need to set this value to false.
+                            IntPtr hWnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
+                            WindowId myWndId = Win32Interop.GetWindowIdFromWindow(hWnd);
+                            var _appWindow = AppWindow.GetFromWindowId(myWndId);
+                            _appWindow.SetPresenter(AppWindowPresenterKind.Overlapped);
+                            if(_appWindow.Presenter is OverlappedPresenter p)
+                            {
+                                p.Maximize();
+                            }
+                        });
+                    });
+#endif
                 });
 
             builder.Services.AddMauiBlazorWebView();

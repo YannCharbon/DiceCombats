c18a5474891a09cace9ac15802a88c6ee6b3bd2c
YannCharbon <yann.charbon@ik.me>
2024-12-23 23:21:31 +0100

    Improve initiative view layout and features

--- DiceCombats/DCCreature.cs
+++ DiceCombats/DCCreature.cs
@@ -23,6 +23,8 @@ namespace DiceCombats
         public List<DCCreatureCustomField> CustomFields { get; set; } = new List<DCCreatureCustomField>();
         public uint InCombatInstanceCount { get; set; } = 1;
         public bool IsPlayer { get; set; } = false;
+        public int InitiativeRank { get; set; } = 0;
+        public int InitiativeRoll { get; set; } = 0;
 
         public DCCreature(string name)
         {

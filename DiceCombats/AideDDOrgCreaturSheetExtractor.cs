d933008ace5917a56517541f17797bdc19e45e7b
YannCharbon <yann.charbon@ik.me>
2026-04-28 19:35:54 +0200

    Fix aidedd.org 2014 stat block rendering issue + add support for 2024 stat blocks

--- DiceCombats/AideDDOrgCreaturSheetExtractor.cs
+++ DiceCombats/AideDDOrgCreaturSheetExtractor.cs
@@ -8,18 +8,21 @@
 
 using HtmlAgilityPack;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace DiceCombats
 {
     public class AideDDOrgCreaturSheetExtractor
     {
-        private string _url;
+        private static readonly HttpClient Http = new HttpClient();
+
+        private readonly string _url;
 
         public string CreatureName { get; private set; } = "N/A";
+
         public AideDDOrgCreaturSheetExtractor(string url)
         {
             _url = url;
@@ -29,73 +32,46 @@ namespace DiceCombats
         {
             try
             {
-                HttpClient client = new HttpClient();   // actually only one object should be created by Application
-                string rawAideDDCreaturePage = await client.GetStringAsync(_url);
+                string rawAideDDCreaturePage = await Http.GetStringAsync(_url);
 
                 HtmlDocument doc = new HtmlDocument();
                 doc.LoadHtml(rawAideDDCreaturePage);
 
-                string creaturSheetDivContent = "";
+                var creatureSheetDiv = doc.DocumentNode.SelectSingleNode(
+                    "//div[contains(concat(' ', normalize-space(@class), ' '), ' col1 ')]");
 
-                // div with class 'col1' stores the creature sheet
-                var creatureSheetdiv = doc.DocumentNode.SelectSingleNode($"//div[contains(@class, 'col1')]");
-                if (creatureSheetdiv != null)
+                if (creatureSheetDiv == null)
+                    return "Error: creature sheet not found";
+
+                creatureSheetDiv = creatureSheetDiv.CloneNode(true);
+
+                bool isDnd2024 = IsDnd2024Sheet(creatureSheetDiv);
+
+                CreatureName = GetH1(creatureSheetDiv);
+
+                RemoveDivByClass(creatureSheetDiv, "trad");
+                RemoveDivByClass(creatureSheetDiv, "description");
+                RemoveDivByClass(creatureSheetDiv, "source");
+                RemoveAllOlElements(creatureSheetDiv);
+                RemoveDirectBrElements(creatureSheetDiv);
+
+                if (isDnd2024)
                 {
-                    CreatureName = GetH1(creatureSheetdiv.InnerHtml);
-
-                    foreach (string line in creatureSheetdiv.InnerHtml.Split("\n"))
-                    {
-                        if (line.Contains("class=\"description\""))
-                        {
-                            break;
-                        }
-                        creaturSheetDivContent += line;
-                    }
-
-                    // Strip some unwanted elements
-                    creaturSheetDivContent = RemoveDivByClass(creaturSheetDivContent, "trad");
-                    creaturSheetDivContent = RemoveDivByClass(creaturSheetDivContent, "description");
-                    creaturSheetDivContent = RemoveDivByClass(creaturSheetDivContent, "source");
-                    creaturSheetDivContent = RemoveAllOlElements(creaturSheetDivContent);
+                    Rename2024Classes(creatureSheetDiv);
                 }
                 else
                 {
-                    creaturSheetDivContent = "Error";
+                    // Avoid conflict with app/global CSS using a generic .red class.
+                    RenameClass(creatureSheetDiv, "red", "red-aidedd");
                 }
 
-                // Get the styles
-                var styles = doc.DocumentNode.SelectNodes("//style");
-                string styleContents = "";
+                string creatureSheetDivContent = creatureSheetDiv.InnerHtml;
 
-                if (styles != null)
-                {
-                    foreach (var style in styles)
-                    {
-                        foreach (string line in style.InnerHtml.Replace("../", "https://www.aidedd.org/").Split('\n'))
-                        {
-                            // Remove styles that could mess up the application styles
-                            if (!(line.Contains("\t*") || line.Contains("html") || line.Contains("body") || line.Contains("\ta") || line.Contains("a:hover") || line.Contains("h1") || line.Contains("\tp") || line.Contains("\tul") || line.Contains("\tli") || line.Contains("\tol") || line.Contains("span") || line.Contains(".col\t")))
-                            {
-                                if (line.Contains(".red"))
-                                {
-                                    // Replace conflicting style
-                                    styleContents += line.Replace(".red", ".red-aidedd") + "\n";
-                                    creaturSheetDivContent = creaturSheetDivContent.Replace("class='red'", "class='red-aidedd'");
-                                }
-                                else
-                                {
-                                    styleContents += line + "\n";
-                                }
-                            }
-                        }
-                    }
-
-                    styleContents += "a:link { color:#6D0000; }\na:hover { color: #6D0000; text-decoration: underline; }\n";
-                }
-
-                
-                // Construct the final html
-                return "<div>" + "<style>" + styleContents + "</style>" + creaturSheetDivContent + "</div>";
+                return
+                    "<div class=\"" + (isDnd2024 ? "aidedd-sheet-2024" : "aidedd-sheet") + "\">" +
+                    "<style>" + GetMonsterSheetCss(isDnd2024) + "</style>" +
+                    creatureSheetDivContent +
+                    "</div>";
             }
             catch (Exception ex)
             {
@@ -103,50 +79,342 @@ namespace DiceCombats
             }
         }
 
-        private string RemoveDivByClass(string html, string divClass)
+        private static bool IsDnd2024Sheet(HtmlNode root)
         {
-            HtmlDocument doc = new HtmlDocument();
-            doc.LoadHtml(html);
-
-            var nodes = doc.DocumentNode.SelectNodes($"//div[contains(@class, '{divClass}')]");
-            if (nodes != null)
-            {
-                foreach (var node in nodes)
-                {
-                    node.ParentNode.RemoveChild(node);
-                }
-            }
-
-            return doc.DocumentNode.InnerHtml;
+            return
+                root.SelectSingleNode(".//div[contains(concat(' ', normalize-space(@class), ' '), ' init ')]") != null ||
+                root.SelectSingleNode(".//div[contains(concat(' ', normalize-space(@class), ' '), ' car1 ')]") != null ||
+                root.SelectSingleNode(".//h2[contains(concat(' ', normalize-space(@class), ' '), ' rub ')]") != null;
         }
 
-        private string RemoveAllOlElements(string html)
+        private static void Rename2024Classes(HtmlNode root)
         {
-            HtmlDocument doc = new HtmlDocument();
-            doc.LoadHtml(html);
+            RenameClass(root, "jaune", "jaune-2024");
+            RenameClass(root, "sansSerif", "sansSerif-2024");
+            RenameClass(root, "red", "red-2024");
+            RenameClass(root, "type", "type-2024");
+            RenameClass(root, "init", "init-2024");
+            RenameClass(root, "rub", "rub-2024");
 
-            var olNodes = doc.DocumentNode.SelectNodes("//ol");
-            if (olNodes != null)
-            {
-                foreach (var node in olNodes)
-                {
-                    node.ParentNode.RemoveChild(node);
-                }
-            }
-
-            return doc.DocumentNode.OuterHtml;
+            RenameClass(root, "car", "car-2024");
+            RenameClass(root, "car1", "car1-2024");
+            RenameClass(root, "car2", "car2-2024");
+            RenameClass(root, "car3", "car3-2024");
+            RenameClass(root, "car4", "car4-2024");
+            RenameClass(root, "car5", "car5-2024");
+            RenameClass(root, "car6", "car6-2024");
         }
 
-        private string GetH1(string html)
+        private static string GetMonsterSheetCss(bool isDnd2024)
         {
-            HtmlDocument doc = new HtmlDocument();
-            doc.LoadHtml(html);
+            return isDnd2024 ? GetMonsterSheetCss2024() : GetMonsterSheetCss2014();
+        }
 
-            var h1Node = doc.DocumentNode.SelectNodes("//h1").First();
+        private static string GetMonsterSheetCss2014()
+        {
+            return @"
+.aidedd-sheet {
+    display: block;
+    width: 100%;
+    color: black;
+    font-family: Georgia, ""Times New Roman"", serif;
+    font-size: 14px;
+    line-height: 1.55;
+}
+
+.aidedd-sheet * {
+    box-sizing: border-box;
+}
+
+.aidedd-sheet .orange {
+    border: 1px black solid;
+    background-color: #e69a28;
+    width: 100%;
+    height: 6px;
+}
+
+.aidedd-sheet .jaune {
+    background-color: #fdf1dc;
+    padding: 10px 8px 5px 8px;
+    margin: 0 2px 0 2px;
+    box-shadow: 0px 0px 6px 0px rgba(0,0,0,0.5);
+    background-image:
+        url(""https://www.aidedd.org/images/fond-mm-effet.png""),
+        url(""https://www.aidedd.org/images/fond-ph.jpg"");
+    background-repeat: no-repeat, repeat;
+}
+
+.aidedd-sheet h1 {
+    margin: 0;
+    padding: 0;
+    font-variant: small-caps;
+    letter-spacing: 1px;
+    color: #6D0000;
+    font-size: 32px;
+    line-height: 1;
+    break-after: avoid;
+}
+
+.aidedd-sheet .sansSerif {
+    font-family: Arial, sans-serif;
+}
+
+.aidedd-sheet .type {
+    padding: 2px 0 0 0;
+    font-size: 14px;
+    font-style: italic;
+}
+
+.aidedd-sheet div.sansSerif svg {
+    width: 100%;
+    height: 5px;
+}
+
+.aidedd-sheet .red-aidedd {
+    color: #6D0000;
+    font-size: 14px;
+}
+
+.aidedd-sheet .carac {
+    display: inline-block;
+    width: 16.6%;
+    max-width: 55px;
+    text-align: center;
+    vertical-align: top;
+}
+
+.aidedd-sheet p {
+    margin: 0;
+    padding: 0;
+    font-size: 14px;
+    line-height: 1.55;
+}
+
+.aidedd-sheet .rub {
+    color: #6D0000;
+    margin: 6px 0 4px 0;
+    border-bottom: 1px solid #6D0000;
+    font-size: 18px;
+    font-weight: normal;
+    font-variant: small-caps;
+}
+
+.aidedd-sheet a {
+    color: #6D0000;
+    text-decoration: none;
+}
+
+.aidedd-sheet a:hover {
+    color: #6D0000;
+    text-decoration: underline;
+}
+";
+        }
+
+        private static string GetMonsterSheetCss2024()
+        {
+            return @"
+.aidedd-sheet-2024 {
+    display: block;
+    width: 100%;
+    color: black;
+    font-family: ""Open Sans"", Arial, sans-serif;
+    font-size: 14px;
+    line-height: 1.55;
+    font-weight: 500;
+}
+
+.aidedd-sheet-2024 * {
+    box-sizing: border-box;
+}
+
+.aidedd-sheet-2024 .jaune-2024 {
+    background-color: #f1eee8;
+    padding: 12px 18px 14px 18px;
+    margin: 0;
+    border: 4px solid #bdbdb8;
+    border-radius: 12px;
+    box-shadow: none;
+    background-image: none;
+}
+
+.aidedd-sheet-2024 h1 {
+    margin: 0 0 4px 0;
+    padding: 0 0 2px 0;
+    color: #6D0000;
+    border-bottom: 1px solid #6D0000;
+    font-family: ""Open Sans"", Arial, sans-serif;
+    font-size: 28px;
+    font-weight: 700;
+    font-variant: small-caps;
+    letter-spacing: 2px;
+    line-height: 1.1;
+}
+
+.aidedd-sheet-2024 .sansSerif-2024 {
+    font-family: ""Open Sans"", Arial, sans-serif;
+}
+
+.aidedd-sheet-2024 .red-2024 {
+    color: #6D0000;
+    font-size: 14px;
+}
+
+.aidedd-sheet-2024 .type-2024 {
+    padding: 0 0 4px 0;
+    color: #555;
+    font-size: 14px;
+    font-style: italic;
+}
+
+.aidedd-sheet-2024 .init-2024 {
+    float: right;
+    margin-left: 12px;
+    white-space: nowrap;
+    color: #6D0000;
+    font-weight: 700;
+}
+
+.aidedd-sheet-2024 .car-2024,
+.aidedd-sheet-2024 .car1-2024,
+.aidedd-sheet-2024 .car2-2024,
+.aidedd-sheet-2024 .car3-2024,
+.aidedd-sheet-2024 .car4-2024,
+.aidedd-sheet-2024 .car5-2024,
+.aidedd-sheet-2024 .car6-2024 {
+    display: inline-block;
+    width: 7.75%;
+    margin: 0 0.15%;
+    padding: 3px 2px;
+    text-align: center;
+    vertical-align: top;
+    line-height: 1.25;
+    font-size: 14px;
+}
+
+.aidedd-sheet-2024 .car-2024 {
+    color: #555;
+    background-color: transparent;
+    font-size: 12px;
+    font-weight: 500;
+}
+
+.aidedd-sheet-2024 .car1-2024,
+.aidedd-sheet-2024 .car2-2024,
+.aidedd-sheet-2024 .car4-2024,
+.aidedd-sheet-2024 .car5-2024 {
+    background-color: #e8e5dc;
+}
+
+.aidedd-sheet-2024 .car3-2024,
+.aidedd-sheet-2024 .car6-2024 {
+    background-color: #d9d4d1;
+}
+
+.aidedd-sheet-2024 .car1-2024,
+.aidedd-sheet-2024 .car4-2024 {
+    color: #6D0000;
+    font-weight: 700;
+    text-transform: uppercase;
+}
+
+.aidedd-sheet-2024 .car2-2024,
+.aidedd-sheet-2024 .car3-2024,
+.aidedd-sheet-2024 .car5-2024,
+.aidedd-sheet-2024 .car6-2024 {
+    color: #6D0000;
+    font-weight: 700;
+}
+
+.aidedd-sheet-2024 p {
+    margin: 0;
+    padding: 0;
+    color: black;
+    font-size: 14px;
+    line-height: 1.55;
+}
+
+.aidedd-sheet-2024 h2.rub-2024,
+.aidedd-sheet-2024 .rub-2024 {
+    color: #6D0000;
+    margin: 10px 0 6px 0;
+    padding: 0 0 2px 0;
+    border-bottom: 1px solid #6D0000;
+    font-size: 20px;
+    font-weight: 500;
+    font-variant: small-caps;
+    letter-spacing: 1px;
+    line-height: 1.2;
+}
+
+.aidedd-sheet-2024 a {
+    color: #6D0000;
+    text-decoration: none;
+}
+
+.aidedd-sheet-2024 a:hover {
+    color: #6D0000;
+    text-decoration: underline;
+}
+";
+        }
+
+        private static void RemoveDivByClass(HtmlNode root, string divClass)
+        {
+            var nodes = root.SelectNodes(
+                $".//div[contains(concat(' ', normalize-space(@class), ' '), ' {divClass} ')]");
+
+            if (nodes == null)
+                return;
+
+            foreach (var node in nodes.ToList())
+                node.Remove();
+        }
+
+        private static void RemoveAllOlElements(HtmlNode root)
+        {
+            var nodes = root.SelectNodes(".//ol");
+
+            if (nodes == null)
+                return;
+
+            foreach (var node in nodes.ToList())
+                node.Remove();
+        }
+
+        private static void RemoveDirectBrElements(HtmlNode root)
+        {
+            var nodes = root.SelectNodes("./br");
+
+            if (nodes == null)
+                return;
+
+            foreach (var node in nodes.ToList())
+                node.Remove();
+        }
+
+        private static void RenameClass(HtmlNode root, string oldClass, string newClass)
+        {
+            var nodes = root
+                .DescendantsAndSelf()
+                .Where(n => n.Attributes["class"] != null);
+
+            foreach (var node in nodes)
+            {
+                var classes = node.GetAttributeValue("class", "")
+                    .Split(new[] { ' ', '\t', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(c => c == oldClass ? newClass : c);
+
+                node.SetAttributeValue("class", string.Join(" ", classes));
+            }
+        }
+
+        private static string GetH1(HtmlNode root)
+        {
+            var h1Node = root.SelectSingleNode(".//h1");
+
             if (h1Node != null)
-            {
-                return h1Node.InnerHtml;
-            }
+                return WebUtility.HtmlDecode(h1Node.InnerText.Trim());
 
             return "N/A";
         }
--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,56 +1,15 @@
 # Release notes
 
-## Detailed popups and richer creature notes
-
-### Fixes
-
-Fix of the masonry layout when rendering Detailed popup sub-contents with kind "text" or "markdown".
-
 ### New
 
-- **“Detailed popup” custom field for creatures**  
-  You can now add a new type of custom field called **DetailedPopup** on your creatures.  
-  This field lets you attach rich “detail sheets” to a creature, instead of squeezing everything into a single text box.
-
-- **Rich content inside popups (text, Markdown, HTML, PDF)**  
-  Inside a Detailed popup you can now store:
-  - Simple text notes  
-  - Markdown notes (for headings, lists, emphasis, etc.)  
-  - HTML “cards” based on templates (for more advanced layouts)  
-  - PDFs (for example: spell cards, stat blocks, handouts, or rules extracts)  
-
-- **Quick search inside a popup**  
-  When you open a detailed popup during a game, you get:
-  - A **search box** to quickly find the right section by name or content  
-  - A **toggle between list mode and grid mode**, so you can either read everything top-to-bottom or browse “cards” at a glance  
-
-- **Grid view for detailed content**  
-  The new grid layout lets you see several pieces of information side by side as “cards”.  
-  This is especially handy for long sheets (e.g. complex monsters, bosses, or NPCs with a lot of lore, abilities, and notes).
+- **Support of aidedd.org DnD 5.5 stat block extraction**
+  You can now import a DnD 2024 creature stat block from aidedd.org, DnD 2014 was already supported.
 
 ### Improvements
 
-- **Better help when creating custom fields**  
-  The creature custom fields editor now shows clearer explanations and examples for each field type, including the new Detailed popup field.  
-  This should make it easier to choose the right field type without needing technical knowledge.
-
-- **More comfortable popup windows**  
-  The in-app popups (modals) have been reworked:
-  - They are easier to read on both large screens and smaller laptops  
-  - Scroll behaviour is smoother when you edit long content  
-  - Dialogs from DiceCombats (confirmation boxes, etc.) now correctly appear **above** other popups instead of hiding behind them
-
-- **Visual polish in the custom fields editor**  
-  Several small visual tweaks make the custom fields area clearer:
-  - Better spacing and alignment  
-  - Clearer grouping of controls  
-  - A slightly wider layout for the field selection and help area, so examples are easier to see
+- Nothing
 
 ### Bug fixes
 
-- **HTML details from file now load correctly**  
-  A problem where HTML-based content loaded from a file would sometimes fail to display has been fixed.  
-  You can now safely keep your HTML snippets or templates in separate files and use them in your Detailed popups.
-
-- **General minor visual fixes**  
-  A few small UI glitches and inconsistencies were cleaned up to make the app feel smoother and more coherent.
+- **Aidedd.org stat block extraction issue**
+  Aidedd.org website changed their way of managing CSS styles of their stat blocks which caused rendering issues in DiceCombats.
